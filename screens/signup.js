8c4ad0d8dec76f04a0b5896c9a80246f09f4a2dc
Nazim Leulmi <leulminaz@outlook.com>
2022-04-26 14:56:34 +0400

    refactoring ( removing styled-components )

--- screens/signup.js
+++ screens/signup.js
@@ -1,24 +1,71 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ImageBackground, TextInput } from "react-native";
 import { COLORS, FONTS, windowHeight, windowWidth } from "./index";
 
-const Container = styled(SafeAreaView)`
+const Background = styled(ImageBackground)`
   flex: 1;
   background-color: whitesmoke;
   position: relative;
+  position: relative;
+`;
+const Container = styled.View`
+  flex: 1;
+  background-color: rgba(255, 255, 255, 0.85);
   padding: 20px;
 `;
 
-const Logo = styled.Image``;
+const Logo = styled.Image`
+  width: 70px;
+  height: 70px;
+  margin-bottom: 30px;
+`;
+
+const Header = styled.Text`
+  font-family: ${FONTS.regular};
+  font-size: 55px;
+  width: 75%;
+  color: ${COLORS.starblue};
+`;
+
+const SubHeader = styled.Text`
+  font-family: ${FONTS.regular};
+  font-size: 22px;
+  color: black;
+  width: 75%;
+  margin-bottom: 30px;
+`;
+
+const Label = styled.Text`
+  font-family: ${FONTS.regular};
+  font-size: 18px;
+  color: black;
+  width: 75%;
+`;
+
+const Input = styled(TextInput)`
+  width: 100%;
+  height: 55px;
+  background-color: whitesmoke;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  margin-top:8px;
+`;
 
 function SignUp() {
   return (
-    <Container>
+    <Background source={require("../assets/background.jpg")} resizeMode="cover">
       <StatusBar style="light" translucent={false} />
-      <Logo source={require("../assets/logo.png")} />
-    </Container>
+      <Container>
+        <Logo source={require("../assets/logo.png")} />
+        <Header>SIGN UP</Header>
+        <SubHeader>
+          Enter a valid email and a strong password to create an account
+        </SubHeader>
+        <Label>EMAIL ADDRESS</Label>
+        <Input />
+      </Container>
+    </Background>
   );
 }
 
